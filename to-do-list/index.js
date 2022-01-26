fd5ba6630d87e21692ba94ee6f5b0c41394a7b30
DannyLenk <danylenko.14@gmail.com>
2022-01-26 10:57:57 +0200

    roughly implemented delete item functionality

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -12,8 +12,7 @@ let view = {
       for (let i = 0; i < tasks.length; i++) {
          ulList.innerHTML += `<li>
             ${i + 1}. ${tasks[i]}
-            <button class="removeBtn">X</button><button class="doneBtn">Done</button>
-         
+            <button class="removeBtn" id="${i}">X</button><button class="doneBtn">Done</button>
          </li>`;
       }
       console.log(tasks);
@@ -21,7 +20,7 @@ let view = {
 };
 
 let controller = {
-   tasks: [],
+   tasks: ['hello', 'world'],
    submitInput: function() {
       let userInput = document.querySelector('#inputField').value;
       if (userInput) {
@@ -29,10 +28,37 @@ let controller = {
          document.querySelector('#inputField').value = '';
       }
       view.renderInput(controller.tasks);
+      console.log(removeButtons);
+      console.log(ulEl);
+
    },
    clearList: function() {
       console.log('here I am');
       controller.tasks = [];
       view.renderInput(controller.tasks);
-   }
+   },
+   // removeItem: function() {
+
+   // }
 };
+
+let removeButtons = document.getElementsByClassName('removeBtn');
+let ulEl = document.querySelector('.output__ul');
+console.log(ulEl);
+
+function removeItem(e) {
+   if (e.target.classList.contains('removeBtn')) {
+      console.log('true');
+      controller.tasks.splice(e.target.id, 1);
+      view.renderInput(controller.tasks);
+   } else if (e.target.classList.contains('doneBtn')) {
+      console.log('else true');
+   } else {
+      console.log('false');
+   }
+   // e.target.style.visibility = "hidden";
+   console.log(e.target);
+}
+ulEl.addEventListener('click', removeItem);
+
+
