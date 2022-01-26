570c1a1060830cc962bf2eebddbdfaadbd604093
DannyLenk <danylenko.14@gmail.com>
2022-01-26 16:06:57 +0200

    built all

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -10,17 +10,30 @@ let view = {
       let ulList = document.querySelector('.output__ul');
       ulList.innerHTML = '';
       for (let i = 0; i < tasks.length; i++) {
-         ulList.innerHTML += `<li>
-            ${i + 1}. ${tasks[i]}
-            <button class="removeBtn" id="${i}">X</button><button class="doneBtn">Done</button>
-         </li>`;
+         if (checkIfDone(tasks[i])) {
+            ulList.innerHTML += `<li>
+               <span class="doneTasks">${i + 1}. ${tasks[i]}</span>
+               <button class="removeBtn" id="${i}">X</button><button class="doneBtn" id="${i}">Done</button>
+            </li>`;   
+         } else {
+            ulList.innerHTML += `<li>
+               <span>${i + 1}. ${tasks[i]}</span>
+               <button class="removeBtn" id="${i}">X</button><button class="doneBtn" id="${i}">Done</button>
+            </li>`;
+         }
       }
-      console.log(tasks);
+   },
+   crossOut: function(element) {
+      let parentElement = element.parentNode;
+      let allTheContent = parentElement.children;
+      allTheContent[0].classList.add('doneTasks');
+      console.log(allTheContent[0]);
    }
 };
 
 let controller = {
    tasks: ['hello', 'world'],
+   doneElements: [],
    submitInput: function() {
       let userInput = document.querySelector('#inputField').value;
       if (userInput) {
@@ -28,7 +41,6 @@ let controller = {
          document.querySelector('#inputField').value = '';
       }
       view.renderInput(controller.tasks);
-      console.log(removeButtons);
       console.log(ulEl);
 
    },
@@ -36,29 +48,33 @@ let controller = {
       console.log('here I am');
       controller.tasks = [];
       view.renderInput(controller.tasks);
-   },
-   // removeItem: function() {
-
-   // }
+      controller.doneElements = [];
+   }
 };
 
-let removeButtons = document.getElementsByClassName('removeBtn');
 let ulEl = document.querySelector('.output__ul');
-console.log(ulEl);
 
 function removeItem(e) {
    if (e.target.classList.contains('removeBtn')) {
-      console.log('true');
       controller.tasks.splice(e.target.id, 1);
       view.renderInput(controller.tasks);
    } else if (e.target.classList.contains('doneBtn')) {
-      console.log('else true');
+      view.crossOut(e.target);
+      controller.doneElements.push(controller.tasks[e.target.id]);
+      console.log(controller.doneElements);
    } else {
       console.log('false');
    }
-   // e.target.style.visibility = "hidden";
-   console.log(e.target);
 }
 ulEl.addEventListener('click', removeItem);
+
+function checkIfDone(task) {
+   for (let i = 0; i < controller.doneElements.length; i++) {
+      if (task === controller.doneElements[i]) {
+         return true;
+      }
+   }
+   return false;
+}
 
 
