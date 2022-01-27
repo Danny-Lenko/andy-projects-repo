bc3512e2752d2ca4fcec062c56582855a578a643
DannyLenk <danylenko.14@gmail.com>
2022-01-27 15:16:26 +0200

    const expenseName = document.querySelector('#name').value;

--- expense-tracker/index.js
+++ expense-tracker/index.js
@@ -5,38 +5,32 @@ window.onload = function() {
 }
 
 let view = {
-   displayExpense: function(name, date, amount) {
+   displayExpense: function() {
       const expenseRowsList = document.getElementsByClassName('output__tr');
-      const args = [name, date, amount, `<button>X</button>`];
 
-      if (expenseRowsList[0].children[3].innerHTML === '') {
-         for (let j = 0; j < expenseRowsList.length; j++) {
-            for (let i = 0; i < expenseRowsList[j].children.length; i++) {
-               expenseRowsList[j].children[i].innerHTML = args[i];
-            }
-         }
-      } else {
-         console.log('queue is busy');
+      if (expenseRowsList[0].children[3].innerHTML) {
          controller.createNewRow();
-         for (let j = 0; j < expenseRowsList.length; j++) {
-            for (let i = 0; i < expenseRowsList[j].children.length; i++) {
-               expenseRowsList[j].children[i].innerHTML = args[i];
-            }
-         }
       }
 
+      for (let i = 0; i < expenseRowsList.length; i++) {
+         expenseRowsList[i].children[0].innerHTML = controller.expensesData[i].name;
+         expenseRowsList[i].children[1].innerHTML = controller.expensesData[i].date;
+         expenseRowsList[i].children[2].innerHTML = controller.expensesData[i].amount;
+         expenseRowsList[i].children[3].innerHTML = controller.expensesData[i].btn;
+      }
    }
 };
 
 let controller = {
-   clicks: 0,
+   expensesData: [],
    addExpense: function() {
       const expenseName = document.querySelector('#name').value;
       const expenseDate = document.querySelector('#date').value;
       const expenseAmount = document.querySelector('#amount').value;
+
       if (expenseName && expenseDate && expenseAmount) {
-         controller.clicks++;
-         view.displayExpense(expenseName, expenseDate, expenseAmount);
+         controller.fetchInputData();
+         view.displayExpense();
 
          document.querySelector('#name').value = '';
          document.querySelector('#amount').value = '';
@@ -47,6 +41,15 @@ let controller = {
       const expenseRow = document.querySelector('.output__tr');
 
       tBody.appendChild(expenseRow.cloneNode(true));
+   },
+   fetchInputData: function() {
+      this.expensesData.push({ 
+         name: document.querySelector('#name').value, 
+         date: document.querySelector('#date').value, 
+         amount: document.querySelector('#amount').value, 
+         btn: `<button>X</button>` 
+      });
+      console.log(this.expensesData[0].name);
    }
 };
 
