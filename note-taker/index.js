651386c8bffaad01342636844ee4b6cd766eef66
DannyLenk <danylenko.14@gmail.com>
2022-01-28 13:51:42 +0200

    set up basic functionality

--- note-taker/index.js
+++ note-taker/index.js
@@ -5,6 +5,12 @@ window.onload = function() {
 }
 
 let view = {
+   addContentToNoteContainer: function(containers) {
+      for (let i = 1; i < containers.length; i++) {
+         containers[i].children[0].innerHTML = `Note ${i}`;
+         containers[i].children[1].innerHTML = controller.notes[i - 1];
+      }
+   }
 
 };
 
@@ -12,20 +18,25 @@ let controller = {
    notes: [],
    addNote: function() {
       const userNote = document.querySelector('#note').value;
-      controller.notes.push(userNote);
 
-      controller.createNoteContainer();
+      if (userNote) {
+         controller.notes.push(userNote);
+         controller.createNoteContainer();
+         document.querySelector('#note').value = "";
+      }
 
    },
    createNoteContainer: function() {
       const outputSection = document.querySelector('#outputSection');
       const noteContainer = document.createElement('div');
+      
       noteContainer.appendChild(document.createElement('h3'));
       noteContainer.appendChild(document.createElement('p'));
       noteContainer.appendChild(document.createElement('button'));
       noteContainer.appendChild(document.createElement('button'));
       outputSection.appendChild(noteContainer);
 
-      console.log(outputSection);
-   }
+      view.addContentToNoteContainer(outputSection.children);
+      console.log(outputSection.children);
+   },
 }
