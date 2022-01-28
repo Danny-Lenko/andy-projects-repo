639f201e2fff0f9331713fa484dd825a6ea4089b
DannyLenk <danylenko.14@gmail.com>
2022-01-28 10:55:05 +0200

    set up the creator function

--- note-taker/index.js
+++ note-taker/index.js
@@ -1,2 +1,31 @@
 "use strict"
 
+window.onload = function() {
+   document.querySelector('#addBtn').addEventListener('click', controller.addNote);
+}
+
+let view = {
+
+};
+
+let controller = {
+   notes: [],
+   addNote: function() {
+      const userNote = document.querySelector('#note').value;
+      controller.notes.push(userNote);
+
+      controller.createNoteContainer();
+
+   },
+   createNoteContainer: function() {
+      const outputSection = document.querySelector('#outputSection');
+      const noteContainer = document.createElement('div');
+      noteContainer.appendChild(document.createElement('h3'));
+      noteContainer.appendChild(document.createElement('p'));
+      noteContainer.appendChild(document.createElement('button'));
+      noteContainer.appendChild(document.createElement('button'));
+      outputSection.appendChild(noteContainer);
+
+      console.log(outputSection);
+   }
+}
