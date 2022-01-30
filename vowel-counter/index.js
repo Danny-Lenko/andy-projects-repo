fc2f64315b3c14f676679c4482cee5ea79347f7e
DannyLenk <danylenko.14@gmail.com>
2022-01-30 05:06:10 +0200

    built all the app

--- vowel-counter/index.js
+++ vowel-counter/index.js
@@ -1,2 +1,13 @@
 "use strict"
 
+document.querySelector('#checkBtn').addEventListener('click', () => {
+   alert(`There are ${fetchAndTransformUserInput().length} vowels`);
+})
+
+function fetchAndTransformUserInput() {
+   const inputText = document.querySelector('#text').value;
+   const regEx = /[aeiou]/ig;
+   const vowelsMatch = inputText.match(regEx);
+   document.querySelector('#text').value = '';
+   return vowelsMatch;
+}
