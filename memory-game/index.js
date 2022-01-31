5d8f4807e7f8737ac49941249c5a96dcc4591d59
DannyLenk <danylenko.14@gmail.com>
2022-01-31 05:07:02 +0200

    added basic functions

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,2 +1,26 @@
 "use strict"
 
+document.querySelector('#picturesCollection').addEventListener('click', manageClicks);
+
+function manageClicks(e) {
+   openPicture(e.target.parentNode.children[1], e.target);
+
+   console.log(e.target.parentNode.children[1].children[0].alt);
+}
+
+function openPicture(toOpen, toClose) {
+   toClose.style.display = "none";
+   toOpen.style.display = "block";
+
+   const timeoutID = window.setTimeout(() => {
+      toClose.style.display = "block";
+      toOpen.style.display = "none";
+   }, 500);
+}
+
+// function checkResult(userChoice) {
+//    const userChoicesArr = [];
+
+//    userChoicesArr.push(userChoice);
+//    if (userChoicesArr[0] === userChoicesArr[1])
+// }
