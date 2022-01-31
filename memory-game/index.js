fcaa34230149e13f75f565425637a95f348b0b00
DannyLenk <danylenko.14@gmail.com>
2022-01-31 06:23:42 +0200

    built double clicks avoidance

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,11 +1,18 @@
 "use strict"
 
+const userChoiceArr = [];
+
 document.querySelector('#picturesCollection').addEventListener('click', manageClicks);
 
 function manageClicks(e) {
-   openPicture(e.target.parentNode.children[1], e.target);
 
-   console.log(e.target.parentNode.children[1].children[0].alt);
+   console.log(e.target);
+
+   if (checkClickDifferent(e.target) && e.target.classList.contains('pictures__green')) {
+      openPicture(e.target.parentNode.children[1], e.target);
+   };
+
+   // console.log(e.target.parentNode.children[1].children[0].alt);
 }
 
 function openPicture(toOpen, toClose) {
@@ -24,3 +31,21 @@ function openPicture(toOpen, toClose) {
 //    userChoicesArr.push(userChoice);
 //    if (userChoicesArr[0] === userChoicesArr[1])
 // }
+
+function checkClickDifferent(userChoice) {
+   const child = userChoice;
+   const parent = child.parentNode;
+   const grandParent = parent.parentNode;
+   const index = Array.prototype.indexOf.call(grandParent.children, parent);
+
+   if (userChoice.classList.contains('pictures__green')) {
+      userChoiceArr.push(index);
+   }
+   console.log(userChoiceArr);
+   if (userChoiceArr[0] !== userChoiceArr[1]) {
+      return true;
+   } else {
+      userChoiceArr.splice(1, 1);
+      return false;
+   }
+}
