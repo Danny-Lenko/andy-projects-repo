4a880bf072edd334d5f6097d113e75eaff7c2b9d
DannyLenk <danylenko.14@gmail.com>
2022-01-31 13:07:03 +0200

    added random array generation function

--- memory-game/index.js
+++ memory-game/index.js
@@ -6,8 +6,16 @@ let score = 0;
 let failed = 0;
 let timeoutID;
 
-document.querySelector('#picturesCollection').addEventListener('click', manageClicks);
+function createPlayground() {
+   const animalsNames = generateRandomSpread();
+   console.log(animalsNames);
 
+   
+
+}
+createPlayground();
+
+document.querySelector('#picturesCollection').addEventListener('click', manageClicks);
 function manageClicks(e) {
    if (checkClickDifferent(e.target) && e.target.classList.contains('pictures__green')) {
       openPicture(e.target.parentNode.children[1], e.target);
@@ -32,7 +40,6 @@ function checkAndShowResult(userChoice) {
       if (userChoiceNamesArr[0] === userChoiceNamesArr[1]) {
          window.clearTimeout(timeoutID);
          userChoiceIndexArr.forEach(index => {
-            console.log(userChoiceIndexArr);
             userChoice.parentNode.parentNode.children[index].children[0].style.display = 'none';
             userChoice.parentNode.parentNode.children[index].children[1].style.display = 'block';
          });
@@ -63,4 +70,18 @@ function checkClickDifferent(userChoice) {
       userChoiceIndexArr.splice(1, 1);
       return false;
    }
+}
+
+function generateRandomSpread() {
+   const imgs = ['cow', 'crab', 'tiger'];
+   const imgsPaired = [...imgs, ...imgs];
+   let randomImgsArr = [];
+   let randomImg;
+
+   for (let i = 0; i < imgs.length * 2; i++) {
+      randomImg = imgsPaired.splice( Math.floor(Math.random() * imgsPaired.length), 1 );
+      randomImgsArr.push(randomImg);
+   }
+   randomImgsArr = randomImgsArr.flat();
+   return randomImgsArr;
 }
